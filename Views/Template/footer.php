4ab6c7c156c06709ba2740014fff3be702b575c8
Cajusol11 <antoniocajusol@gmail.com>
2026-05-26 18:27:01 -0500

    Se actualizo modulo clientes con validaciones y SweetAAlert2

--- Views/Template/footer.php
+++ Views/Template/footer.php
@@ -2,12 +2,13 @@
 
     <div id="contenedorModal"></div>
     <?php $page_js = $data['page_js'] ?? []; ?>
+    <?php $versionCache = time(); ?>
 
-    <script src="<?= BASE_URL ?>public/js/Notificaiones.js"></script>
-    <script src="<?= BASE_URL ?>public/js/Nav.js"></script>
+    <script src="<?= BASE_URL ?>public/js/Notificaiones.js?v=<?= $versionCache ?>"></script>
+    <script src="<?= BASE_URL ?>public/js/Nav.js?v=<?= $versionCache ?>"></script>
     <?php if (!empty($page_js)): ?>
         <?php foreach ($page_js as $js): ?>
-            <script src="<?= BASE_URL ?>public/js/<?= $js ?>"></script>
+            <script src="<?= BASE_URL ?>public/js/<?= $js ?>?v=<?= $versionCache ?>"></script>
         <?php endforeach; ?>
     <?php endif; ?>
     <script src="<?= BASE_URL ?>main.js"></script>
